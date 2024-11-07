282e728bbb0d72a0af362d0c6fa4739044dbf84d
LouisBstar <louisbouvard16@gmail.com>
2024-11-07 17:53:05 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from 'react';
-import ReactDOM from 'react-dom';
-import HabitTracker from './utils.js';
-
-const App = () => <HabitTracker />;
-
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement)
